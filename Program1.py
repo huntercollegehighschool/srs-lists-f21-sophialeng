d2b829fc86633bdd1b3169a5be7b04f3cd8aa78f
sophialeng <74571475+sophialeng@users.noreply.github.com>
2021-11-12 17:32:03 +0000

    Completed Program 1 (Number of Odds)

--- Program1.py
+++ Program1.py
@@ -5,4 +5,8 @@ PROGRAM 1
 Define a function number_of_odds that takes a list of integers as an argument. The function returns how an integer, how many odd numbers are in the list.
 '''
 def number_of_odds(lst):
-  pass #delete this line when you start writing your code
+  var = 0 
+  for i in lst:
+    if i%2 != 0:
+      var += 1 
+  return var 
